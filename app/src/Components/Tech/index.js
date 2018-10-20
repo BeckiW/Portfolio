4135a49fbe973db779cb0ecc8f88923a145442ea
Becki Wordsworth <r.a.wordsworth@gmail.com>
2018-10-21 00:06:33 +0200

    Extra little changes to the wording

--- app/src/Components/Tech/index.js
+++ app/src/Components/Tech/index.js
@@ -10,10 +10,12 @@ class Tech extends React.Component {
       <div className="skills-Component">
 
         <p className="Tech-blurb">
-        I have always had a passion for solving problems with technology. Throughout my PhD and research career I built small solutions and analyse data with a variety of programming tools.
+        I have always had a passion for solving problems with technology. Throughout my PhD and research career I built small solutions and analysed data with a variety of programming tools.
         <br/>
           <br/>
-      Now I am moving from hobby programmer to professional and focussing on Javascript. With the Technigo program I will complete 480 hours of coding, 50+ individual projects with HTML5, CSS, SASS, Javascript (ES6), React, Node, and MongoDB and 6 sprints and group projects with companies such as Volumental, Bonnier, Comprend and Svenska Spel all within 3 months.   <br/>  <br/>In my spare time I also like to build small applications with Python and Swift, and am investigating data visualiston with packages such as D3.js.
+      Now I am moving from hobby programmer to professional and focussing on Javascript. With the Technigo program I will complete 480 hours of coding, 50+ individual projects with HTML5, CSS, SASS, Javascript (ES6), React, Node, and MongoDB and 6 sprints and group projects with companies such as Volumental, Bonnier, Comprend and Svenska Spel all within 3 months. <br/>
+
+      <br/>In my spare time I also like to build small applications with Python and Swift, and research and improve my knowlegde and skills in the area of machine learning and AI.
         </p>
 
       </div>
